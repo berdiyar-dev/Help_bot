6007c1976d430705b0c7cc79372f81ea336906fa
berdiyar-desktop <berdiyare@gmail.com>
2023-06-21 15:08:37 +0500

    help bot

---
+++ lesson.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN_API = "6298535375:AAEiCzlEAz7qo6nnLRf9K0SIv_-buebW7GM"
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text=message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
